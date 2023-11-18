ba3be010ae71cc1de38a81ea2a4788bc0a1881bc
TMeyne <TMeyne@huth.org>
2023-11-18 16:14:17 +0100

    finished add question Screen (task f9)

--- lib/presentation/add_question_screen/widgets/question_form.dart
+++ lib/presentation/add_question_screen/widgets/question_form.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_form_builder/flutter_form_builder.dart';
+import 'package:learn_flutter_1_udemy_tasks/classes/question_classes.dart';
 import 'package:learn_flutter_1_udemy_tasks/presentation/add_question_screen/widgets/quiz_form_field.dart';
 
 class QuestionForm extends StatefulWidget {
@@ -17,24 +18,45 @@ class QuestionFormState extends State<QuestionForm> {
     return Column(
       mainAxisSize: MainAxisSize.min,
       children: [
-        Form(
+        FormBuilder(
           key: _formKey,
-          child: const Wrap(
+          child: Wrap(
             spacing: 20,
             runSpacing: 20,
             children: [
-              QuizFormField(
+              const QuizFormField(
                 inputName: "question",
               ),
-              QuizFormField(
+              const QuizFormField(
                 inputName: "answer",
               ),
-              QuizFormField(
+              const QuizFormField(
                 inputName: "first hint",
               ),
-              QuizFormField(
+              const QuizFormField(
                 inputName: "second hint",
               ),
+              FormBuilderDropdown(
+                name: "Question categorie", 
+                isExpanded: false,
+                items: [
+                for (QuestionCategorie categorie in QuestionCategorie.values)
+                  DropdownMenuItem(
+                    value: categorie,
+                    child: Text(categorie.categorieName),
+                    // Row(
+                    //   mainAxisSize: MainAxisSize.min,
+                    //   children: [
+                    //     CircleAvatar(
+                    //       backgroundImage: AssetImage(
+                    //           'assets/questioncategories/${categorie.categorieName}.png'),
+                    //       radius: 10,
+                    //     ),
+                    //     Text(categorie.categorieName),
+                    //   ],
+                    // ),
+                  )
+              ])
             ],
           ),
         ),
@@ -45,7 +67,14 @@ class QuestionFormState extends State<QuestionForm> {
               _formKey.currentState!.save();
               if (_formKey.currentState!.validate()) {
                 final formData = _formKey.currentState?.value;
-                print(formData);
+                final question = Question(
+                  questionText: formData?['question'],
+                  firstHint: formData?['first hint'],
+                  secondHint: formData?['second hint'],
+                  questionAnswer: formData?['answer'],
+                  questionCategorie: formData?['Question categorie'],
+                );
+                questions.add(question);
               }
             },
             child: const Text('Submit'),
